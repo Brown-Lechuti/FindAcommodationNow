557f4dcd8eb39489344c08801c73a2b692836cf5
BrownLechuti <brownlechuti@gmail.com>
2021-12-27 02:00:35 +0200

    Image retrival fromAzure, updating interface for images

--- Areas/Identity/Pages/Account/Manage/Residence.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/Residence.cshtml.cs
@@ -6,64 +6,79 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace FindAcommodationNow.Areas.Identity.Pages.Account.Manage
 {
     public class ResidenceModel : PageModel
     {
+        /*        private readonly UserManager<IdentityUser> _userManager;
+                private readonly SignInManager<IdentityUser> _signInManager;
+
+                public void OnGet()
+                {
+                }
+
+                //
+                 <div class="row">
+                     <div class="col container">
+                         <div class="col-10">
+                             @foreach (var items in Model.ImageList)
+                             {
+                                 var iPhotoUrl = items;
+                                 <img class="float-left p-2" src="@iPhotoUrl" height="150" />
+                             }
+                         </div>
+                     </div>
+                 </div>
+        */
+
+        private IConfiguration _configuration;
         private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signInManager;
 
-        public void OnGet()
+        public ResidenceModel(IConfiguration Configuration)
         {
+            _configuration = Configuration;
         }
-        
-        /* private IConfiguration _configuration;
-         private readonly UserManager<IdentityUser> _userManager;
-         private readonly SignInManager<IdentityUser> _signInManager;
-
-         public ResidenceModel(IConfiguration Configuration)
-         {
-             _configuration = Configuration;
-         }
 
 
-         [BindProperty]
-         public List<string> ImageList { get; set; }
-         public async Task<IActionResult> OnGetAsync()
-         {
-             string userid = User.Identity.Name;
-             string newString = userid.Replace("@", string.Empty);
-             string uniqueContiner_ = newString.Replace(".", string.Empty); 
+        [BindProperty]
+        public List<string> ImageList { get; set; }
+        public async Task<IActionResult> OnGetAsync()
+        {
+            string userid = User.Identity.Name;
+            string newString = userid.Replace("@", string.Empty);
+            string uniqueContiner_ = "brownlechutigmailcom";
 
-             BlobContinuationToken continuationToken = null;
-             string blobstorageconnection = "DefaultEndpointsProtocol=https;AccountName=storebbl;AccountKey=0i1Fv/O3avNyTTehyTKqGcQjmuyivvDT6H9gNCx8dCmIZGNfPIqCeoIPANXTDW+WuvFqoq3pEGnjyFW7eKtmzA==;EndpointSuffix=core.windows.net";
+            BlobContinuationToken continuationToken = null;
+            string blobstorageconnection = "DefaultEndpointsProtocol=https;AccountName=storebbl;AccountKey=0i1Fv/O3avNyTTehyTKqGcQjmuyivvDT6H9gNCx8dCmIZGNfPIqCeoIPANXTDW+WuvFqoq3pEGnjyFW7eKtmzA==;EndpointSuffix=core.windows.net";
 
-             CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse(blobstorageconnection);
-             CloudBlobClient blobClient = cloudStorageAccount.CreateCloudBlobClient();
-             CloudBlobContainer container = blobClient.GetContainerReference(uniqueContiner_);
+            CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse(blobstorageconnection);
+            CloudBlobClient blobClient = cloudStorageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference(uniqueContiner_);
 
-             var blobs = blobClient.GetContainerReference(uniqueContiner_).ListBlobsSegmentedAsync(continuationToken);
-             var result = blobs.Result;
-             continuationToken = result.ContinuationToken;
-             var images = result.Results.ToList();
+            var blobs = blobClient.GetContainerReference(uniqueContiner_).ListBlobsSegmentedAsync(continuationToken);
+            var result = blobs.Result;
+            continuationToken = result.ContinuationToken;
+            var images = result.Results.ToList();
 
-             ImageList = new List<string>();
+            ImageList = new List<string>();
 
-             foreach (var blobItem in images)
-             {
-                 // A flat listing operation returns only blobs, not virtual directories.
-                 var blob = (CloudBlockBlob)blobItem;
+            foreach (var blobItem in images)
+            {
+                // A flat listing operation returns only blobs, not virtual directories.
+                var blob = (CloudBlockBlob)blobItem;
                 /// blob.Properties.ContentType = "image/jpeg";
-                 _ = blob.SetPropertiesAsync();
-                if (blobItem.GetType() == typeof(CloudBlobDirectory)
+                _ = blob.SetPropertiesAsync();
                  ImageList.Add($"{blob.Uri}");
 
-             }
+            }
 
-             return Page();
-         }
+            return Page();
+        }
 
-     }*/
+
     }
 }
