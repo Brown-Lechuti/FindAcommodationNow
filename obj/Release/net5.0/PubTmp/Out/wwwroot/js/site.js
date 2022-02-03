4972aa7ecdc24d2c374d7c9fedea0bd2f67f5abc
BrownLechuti <brownlechuti@gmail.com>
2022-02-03 15:06:02 +0200

    Remove .gitignore file  for testing

---
+++ obj/Release/net5.0/PubTmp/Out/wwwroot/js/site.js
@@ -0,0 +1,178 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+/*
+var modal = document.getElementById("_myModal"),
+body = document.getElementsByTagName("body"),
+container = document.getElementById("myContainer"),
+btnOpen = document.getElementById("btnApply"), btnClose = document.getElementById("closeModal");
+btnOpen.onclick = function ()
+{
+    modal.className = "Modal is-visuallyHidden", setTimeout(function ()
+    { container.className = "MainContainer is-blurred", modal.className = "Modal" }, 100), container.parentElement.className = "ModalOpen"
+},
+    btnClose.onclick = function ()
+    {
+        modal.className = "Modal is-hidden is-visuallyHidden", body.className = "", container.className = "container", container.parentElement.className = ""
+    }, window.onclick = function (e) { e.target == modal && (modal.className = "Modal is-hidden", body.className = "", container.className = "container", container.parentElement.className = "") };
+*/
+var mbtn = document.getElementById("applyBtn");
+var res1Btn = document.getElementById("res1Btn");
+var res2Btn = document.getElementById("res2Btn");
+var res3Btn = document.getElementById("res3Btn");
+var res4Btn = document.getElementById("res4Btn");
+var res5Btn = document.getElementById("res5Btn");
+var pillsNav = document.getElementById("pillsNav");
+var rowNav = document.getElementById("rowNav");
+
+
+var picsNav = document.getElementById("picsNav");
+var picTitle = document.getElementById("picTitle");
+
+function hideOtherBtns(showingBtn)
+{
+    
+    res1Btn.hidden = true;
+    res2Btn.hidden = true;
+    res3Btn.hidden = true;
+    res4Btn.hidden = true;
+    res5Btn.hidden = true;
+    showingBtn.hidden = false;
+}
+function hideContent() {
+    res1Btn.hidden = true;
+    res2Btn.hidden = true;
+    res3Btn.hidden = true;
+    res4Btn.hidden = true;
+    res5Btn.hidden = true;
+    mbtn.hidden = true;
+
+}
+
+
+function activeTab()
+{
+    
+    var label = $('#_select :selected').text();
+    return label;
+}
+
+$(document).ready(function () {
+
+
+    $('#_select').change(function ()// when the user choose a specific res, show the btn for the res
+    {
+        hideContent();
+        var label = $('#_select :selected').val();
+        if (label != "0") {
+            if (label == "1") {
+                hideOtherBtns(res1Btn);
+            }
+            if (label == "2") {
+                hideOtherBtns(res2Btn);
+            }
+            if (label == "3") {
+                hideOtherBtns(res3Btn);
+            }
+            if (label == "4") {
+                hideOtherBtns(res4Btn);
+            }
+            if (label == "5") {
+                hideOtherBtns(res5Btn);
+            }
+
+            mbtn.hidden = false;
+            mbtn.innerText = "Apply to " + $('#_select :selected').text();
+        }
+           
+    });
+
+});
+
+
+
+function funcPass()
+{
+    var input_ = document.getElementById("myPass");
+    if (input_.type == "password") {
+        input_.type = "text";
+    }
+    else
+    {
+        input_.type = "password";
+    }
+
+}
+
+// Get the modal
+
+
+var _modal = document.getElementById("myModal");
+
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+var images = document.querySelectorAll('img');
+/*for (var i = 0, len = images.length; i < len; i++) {
+    images[i].addEventListener('click', openModal(this));
+}*/
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    _modal.style.display = "none";
+}
+
+function openModal(img) {
+    _modal.style.display = "block";
+    modalImg.src = img.src;
+    captionText.innerHTML = activeTab();
+
+}
+/*Javascript for the form modal */
+//Get the modal
+var modal = document.getElementById("_myModal");
+
+// Get the main container and the body
+var body = document.getElementsByTagName("body");
+var container = document.getElementById("myContainer");
+
+// Get the open button
+var btnOpen = document.getElementById("btnApply");
+
+// Get the close button
+var btnClose = document.getElementById("closeModal");
+
+// Open the modal
+btnOpen.onclick = function () {
+    modal.className = "Modal is-visuallyHidden";
+    setTimeout(function () {
+        container.className = "MainContainer ";
+        modal.className = "Modal";
+    }, 100);
+    container.parentElement.className = "ModalOpen";
+}
+
+// Close the modal
+btnClose.onclick = function () {
+    modal.className = "Modal is-hidden is-visuallyHidden";
+    body.className = "";
+    container.className = "MainContainer";
+    container.parentElement.className = "";
+}
+
+/*var smallCol = document.getElementById("smallCol");
+var bigCol = document.getElementById("bigCol");
+function arrangeCols() {
+    bigCol.className = "col-md-7";
+    smallCol.hidden = false;
+}
+function display() {
+    smallCol.hidden = false;
+}*/
+/*function growSize(icon) {
+    icon.size 
+}*/
